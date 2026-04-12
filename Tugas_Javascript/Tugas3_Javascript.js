7e89a466b2b3ce9acb8162105607323e0bdaa168
dwikikurniawanedu <dwik23235ti@student.nurulfikri.ac.id>
2026-04-13 00:28:17 +0700

    add Tugas3_Javascript

--- Tugas_Javascript/Tugas3_Javascript.js
+++ Tugas_Javascript/Tugas3_Javascript.js
@@ -1,34 +1,41 @@
-const produkToko = [
+let produkToko = [
     {id: 1, nama: "Laptop", harga: 7000000, stok: 5},
     {id: 2, nama: "Mouse", harga: 200000, stok: 10},
     {id: 3, nama: "Keyboard", harga: 350000, stok: 7}
 ]
 
-const tambahProduk = (nama, harga, stok) => {
-    const idBaru = produkToko.length > 0 ? produkToko.at(-1).id + 1 : 1;
-    
-    const produkBaru = { id: idBaru, nama, harga, stok };
+function tambahProduk(nama, harga, stok) {
+    let idBaru = produkToko.length > 0
+    ? produkToko[produkToko.length - 1].id + 1 : 1;
 
+    let produkBaru = {
+        id: idBaru,
+        nama: nama,
+        harga: harga,
+        stok: stok
+    }
     produkToko.push(produkBaru);
     console.log("Produk berhasil ditambahkan:", produkBaru);
 }
 
-const hapusProduk = (id) => {
-    const index = produkToko.findIndex(produk => produk.id === id);
-    
+function hapusProduk(id) {
+    let index = produkToko.findIndex(produk => produk.id === id);
+
     if (index !== -1) {
-        const produkHapus = produkToko.splice(index, 1);
-        console.log("Produk berhasil dihapus:", produkHapus[0]);
+        let produkDihapus = produkToko.splice(index, 1)[0];
+        console.log("Produk berhasil dihapus:", produkDihapus);
     } else {
-        console.log("Produk dengan ID", id, "tidak ditemukan!");
+        console.log("Produk dengan ID", id, "tidak ditemukan.");
     }
 }
 
-const tampilkanProduk = () => {
+function tampilkanProduk() {
     console.log("=== Daftar Produk ===");
-    
+
     produkToko.forEach(produk => {
-        console.log(`ID: ${produk.id}, Nama: ${produk.nama}, Harga: ${produk.harga}, Stok: ${produk.stok}`);
+        console.log(
+            `ID: ${produk.id} | Nama: ${produk.nama} | Harga: ${produk.harga} | Stok: ${produk.stok}`
+        );
     });
 }
 
