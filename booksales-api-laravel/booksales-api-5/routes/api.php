481a57e42a41257101c4a972aea70e98b45f29c9
dwikikurniawanedu <dwik23235ti@student.nurulfikri.ac.id>
2026-05-17 00:18:44 +0700

    Feat: Push Tugas Laravel 5

---
+++ booksales-api-laravel/booksales-api-5/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\GenreController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/books', [BookController::class, 'index']);
+Route::post('/books', [BookController::class, 'store']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::delete('/books/{id}', [BookController::class, 'destroy']);
+
+Route::get('/genres', [GenreController::class, 'index']);
+Route::post('/genres', [GenreController::class, 'store']);
+Route::get('/genres/{id}', [GenreController::class, 'show']);
+Route::delete('/genres/{id}', [GenreController::class, 'destroy']);
+
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::post('/authors', [AuthorController::class, 'store']);
+Route::get('/authors/{id}', [AuthorController::class, 'show']);
+Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
